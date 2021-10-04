fa3cf8205a9103ce96bd64de137fb5f4d11c65ad
hyperchessbot <hyperchessbot@gmail.com>
2021-10-04 09:10:45 +0000

    Test page

---
+++ test/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  Component test ( should print "vue lazy load" in 30px ):<hr>
+  <vuelazyload/>
+  </div>
+</template>
+
+<script>
+//import vuelazyload from "../../dist/vue-lazy-load.esm.js"
+//import "../../dist/vue-lazy-load.css"
+import vuelazyload from "@publishvue/vuelazyload"
+import "@publishvue/vuelazyload/dist/vue-lazy-load.css"
+export default {
+  components:{
+    vuelazyload
+  }
+}
+</script>
